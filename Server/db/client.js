c46e1a7e5ab28582851d30523196f230d8b73526
lcontr1 <contrerasliliana22@gmail.com>
2024-04-28 23:54:29 -0500

    inital commit

--- Server/db/client.js
+++
@@ -1,6 +0,0 @@
-const { Client } = require("pg");
-require("dotenv").config({ path: "./.env"});
-
-const client = new Client(process.env.DB_URL);
-
-module.exports = client;
---
+++ SquareElotero/src/components/Home.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+
+    return (
+        <>
+          <p className="text-blue">Encuentra tu Elotero</p>  
+        </>
+    )
+}
